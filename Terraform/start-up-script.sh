01c4bf5c6c6ed8d96f510fe31b7258679b6a3dc6
vkt1414 <115020590+vkt1414@users.noreply.github.com>
2023-04-22 22:07:09 -0400

    Create start-up-script.sh

---
+++ Terraform/start-up-script.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo \
+  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+
+
+
+# Start the Jupyter notebook container without token authentication
+docker run -d -p 8888:8888 jupyter/scipy-notebook start-notebook.sh --NotebookApp.token=''
